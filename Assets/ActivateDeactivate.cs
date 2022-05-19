5a80178a687a63aa8fe1c46da953432cdedfc6f9
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-05-19 08:51:29 -0300

    Initial commit

---
+++ Assets/ActivateDeactivate.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateDeactivate : MonoBehaviour {
+
+    public GameObject inputField;
+    public bool pressed;
+
+    private void Start()
+    {
+        pressed = false;
+    }
+    public void ActivateAndDeactivate()
+    {
+        if (pressed)
+        {
+            inputField.SetActive(true);
+            pressed = false;
+        }
+        if (!pressed)
+        {
+            inputField.SetActive(false);
+            pressed = true;
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
