bc140d94c30c49ef91a9b1308e89cf09b49f639e
Juan Henriquez <88837385+JuanHenriquez24@users.noreply.github.com>
2022-05-23 10:43:21 -0300

    Did both platforms and the spawns

---
+++ Assets/PlataformScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformScript : MonoBehaviour {
+
+    public float speed;
+    public float limitRightX;
+    public float limitLeftX;
+    public bool toRight;
+    public GameObject cilindro;
+    public GameObject esfera;
+
+    void Start()
+    {
+        toRight = true;
+    }
+
+    void Update()
+    {
+       
+        if (toRight == true)
+        {
+            transform.position += new Vector3(speed, 0, 0);
+        }
+        else
+        {
+            transform.position -= new Vector3(speed, 0, 0);
+        }
+
+        if (transform.position.x > limitRightX)
+        {
+            toRight = false;
+            Instantiate(esfera);
+        }
+        else if(transform.position.x < limitLeftX)
+        {
+            toRight = true;
+            Instantiate(cilindro);
+        }
+    }
+}
